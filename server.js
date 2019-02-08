3b32cd0ff8951adfbea715443824a0468dfa383f
Julio Francis Pereira dos Santos Mendonça <julio_fpsm@hotmail.com>
2019-02-08 12:19:19 -0200

    subindo mudanças.

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
-var express = require('express'),
-    app = express();
-
-app.use(express.static('www'));
-
-// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
-
-// API Routes
-// app.get('/blah', routeHandler);
-
-app.set('port', process.env.PORT || 5000);
-
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+var express = require(‘express’);
+var path = require(‘path’)
+var app = express();
+app.use(express.static(path.resolve(__dirname, “www”)));
+app.set(‘port’, process.env.PORT || 3000);
+app.listen(app.get(‘port’), function() {
+ console.log(“listening to Port”, app.get(“port”));
 });
