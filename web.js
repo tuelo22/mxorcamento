dc05d6990fb5a893a0fd4e80a1e690f42e31b9e3
tuelo22 <julio_fpsm@hotmail.com>
2019-02-09 10:22:52 -0200

    subindo modificação

--- web.js
+++
@@ -1,4 +0,0 @@
-var express = require(‘express’);
-var app = express();
-app.use(express.static(__dirname + ‘src’)); //aqui você define onde está o index.html da sua aplicação.
-app.listen(process.env.PORT || 3000);
